a8c88c29ff1b25a904ec75aff39782cf7a3e0f57
Rafael Azevedo <rafaellucasaz94@gmail.com>
2024-10-05 22:17:11 -0300

    semi finalizado

--- cod/a.py
+++ cod/a.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 dataPath = "../dataset/n"
 
@@ -21,18 +25,54 @@ validation_generator = dataGen.flow_from_directory(
     subset='validation'
 )
 
+model = Sequential()
 
 
-for batch_images, batch_labels in train_generator:
-    # Exemplo: Acessando a primeira imagem do lote
-    first_image = batch_images[0]  # A primeira imagem (forma: (28, 28, 3))
-    
-    # Verificando os dados da primeira imagem
-    print("Dados da primeira imagem:")
-    print(first_image[:28])  # Exibirá os valores normalizados entre 0.0 e 1.0
-    print("Forma da imagem:", first_image.shape)  # Deve ser (28, 28, 3)
-    
-    # Exibindo os rótulos correspondentes
-    first_label = batch_labels[0]  # O rótulo correspondente
-    print("Rótulo da primeira imagem:", first_label)
-    break;  # Um vetor de 10 elementos representando a classe
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+
+
+model.add(Dense(15, activation='softmax'))
+
+# Compilar o modelo
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+model.summary()
+
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // validation_generator.batch_size,
+    epochs=10 
+)
+
+
+loss, accuracy = model.evaluate(validation_generator)
+print(f'Acurácia do modelo: {accuracy * 100:.2f}%')
+
+
+
+
+
+img_path = '../image.png'
+img = image.load_img(img_path, target_size=(28, 28))
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)  
+img_array /= 255.0  
+
+
+prediction = model.predict(img_array)
+predicted_class = np.argmax(prediction)  
+print(f'O modelo previu o dígito: {predicted_class}')
